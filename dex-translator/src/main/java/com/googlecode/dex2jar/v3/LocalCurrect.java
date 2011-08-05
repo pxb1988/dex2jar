e78398ed32106c1c71bc47a1d5058fe3c8e5b9dd
pxb1988 <none@none>
2011-08-06 01:46:28 +0800

    bug fix
    --HG--
    branch : 0.0.9.x

--- dex-translator/src/main/java/com/googlecode/dex2jar/v3/LocalCurrect.java
+++ dex-translator/src/main/java/com/googlecode/dex2jar/v3/LocalCurrect.java
@@ -95,7 +95,7 @@ public class LocalCurrect implements Transformer {
         Type t2 = LocalType.type(e2.op1.value);
         if (t2 == null && e2.op1.value.vt == VT.ARRAY) {
             Type t3 = detectArray((E2Expr) e2.op1.value);
-            if (t3 != null && t3.getSize() == Type.ARRAY) {
+            if (t3 != null && t3.getSort() == Type.ARRAY) {
                 Type t4 = Type.getType(t3.getDescriptor().substring(1));
                 LocalType.type(e2, t4);
                 return t4;
