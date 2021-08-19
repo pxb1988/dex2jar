4720fb87c94ed9572d1f330fd1af6ba55892f326
Nico Mexis <nico.mexis@kabelmail.de>
2021-08-19 22:11:59 +0200

    Remove Java 6 compat
    It didn't work anyways.... Besides, you should definetly NOT use Java 6 nowadays
    
    (cherry picked from commit 3a13f99df3d7b42fe04829af721cde75001405d6)

--- d2j-j6/src/main/java/pxb/java/nio/file/FileSystem.java
+++
@@ -1,7 +0,0 @@
-package pxb.java.nio.file;
-
-import java.io.Closeable;
-
-public abstract class FileSystem implements Closeable {
-    public abstract Path getPath(String first, String... more);
-}
--- d2j-j6/src/main/java/pxb/java/nio/file/FileVisitResult.java
+++
@@ -1,6 +0,0 @@
-package pxb.java.nio.file;
-
-
-public enum  FileVisitResult {
-    CONTINUE,TERMINATE,SKIP_SUBTREE, SKIP_SIBLINGS;
-}
